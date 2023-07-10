8cd28392387c709e84eb5a2454518e26f8dc6e87
entpnrusman <entpnr.usman@gmail.com>
2023-07-10 11:07:25 +0500

    test

--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -29,7 +29,7 @@ export function UserNav() {
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">shadcn</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              me@example.com
             </p>
           </div>
         </DropdownMenuLabel>
